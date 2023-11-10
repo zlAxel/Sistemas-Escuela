83597f756af3a90afb81ae53ade18f0ea5e15e43
zlAxel <axel.macias17@hotmail.com>
2023-11-09 22:01:33 -0600

    Se sube actividad 1

---
+++ Programacion 1/actividad_1.java
@@ -0,0 +1,15 @@
+public class ejemplo2141{
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in)
+        “float sue,nsue = 0;”
+        System.out.print("ingrese sueldo: ");
+        sue=teclado.nextFloat();
+        (sue != 1000)
+        if(sue<1000){
+            nsue = (float) (sue * “1.15”);
+            if(sue>1000)
+                nsue= (float) (sue * 1.12);
+                System.out.printin("el sueldo final es: "+nsue);
+        }
+        if(sue==1000)
+            System.out.printin("el sueldo final es: "+sue)
