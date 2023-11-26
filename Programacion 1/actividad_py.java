7ec510466c1bf28774d4f68cb80b56cca5fe4af8
zlAxel <axel.macias17@hotmail.com>
2023-11-25 23:25:07 -0600

    Se sube actividad del proyecto modular

---
+++ Programacion 1/actividad_py.java
@@ -0,0 +1,9 @@
+public class actividad_py {
+    public static void main(String[] args) {
+        // ! Utilizando un bucle for
+        System.out.println("Utilizando un bucle for:");
+        for (int i = 0; i <= 9; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
